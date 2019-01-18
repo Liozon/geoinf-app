0fb14b593f4b971ee38e77e0927fd72313ec32b6
Julien Muggli <liozon@hotmail.com>
2019-01-18 09:05:20 +0100

    Formatted some files

---
+++ api/api.sample.js
@@ -0,0 +1,16 @@
+// Thunderforest API key
+var thunderKey = "YOUR_API_KEY";
+
+// Swiss Confederation layers
+var mapURL = 'https://wms.geo.admin.ch';
+
+// Weather layer
+var weatherKey = 'YOUR_API_KEY';
+
+// Here Maps keys
+var app_id = "YOUR_API_KEY";
+var app_code = "YOUR_API_KEY";
+
+// CartoDB/Carto.com API key and format definition
+var format = "GeoJSON";
+var key = "YOUR_API_KEY";
