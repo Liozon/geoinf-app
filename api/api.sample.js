905b2d84f411f722b3f3fb6d21f664e752aa273a
Julien Muggli <liozon@hotmail.com>
2019-01-20 17:33:58 +0100

    Update api.sample.js

--- api/api.sample.js
+++ api/api.sample.js
@@ -1,16 +1,20 @@
 // Thunderforest API key
+// Get your key here: https://manage.thunderforest.com
 var thunderKey = "YOUR_API_KEY";
 
 // Swiss Confederation layers
 var mapURL = 'https://wms.geo.admin.ch';
 
 // Weather layer
+// Get your key here: https://home.openweathermap.org/users/sign_up
 var weatherKey = 'YOUR_API_KEY';
 
 // Here Maps keys
+// Get your keys here: https://developer.here.com/authenticationpage
 var app_id = "YOUR_API_KEY";
 var app_code = "YOUR_API_KEY";
 
 // CartoDB/Carto.com API key and format definition
+// Get your key here: https://carto.com/login
 var format = "GeoJSON";
 var key = "YOUR_API_KEY";
